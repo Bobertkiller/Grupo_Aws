a8938cea88c8079c5b12dd6cb01e320820fa886f
Bobertkiller <felipe.mb142@gmail.com>
2023-08-31 16:29:18 -0300

    Ex12 commit

---
+++ Lab01b/Exercicos/Ex12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int encontrarCaractere(const char *string, char caractere) {
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == caractere) {
+            return i; 
+        }
+    }
+    
+    return -1; 
+}
+
+int main(void) {
+    char *minhaString = "Exemplo de string";
+    char meuCaractere = 'd';
+    
+    int posicao = encontrarCaractere(minhaString, meuCaractere);
+    
+    if (posicao != -1) {
+        printf("O caractere '%c' foi encontrado na posição %d da string.\n", meuCaractere, posicao);
+    } else {
+        printf("O caractere '%c' não foi encontrado na string.\n", meuCaractere);
+    }
+    
+}

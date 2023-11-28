d2d9b83254af5b25ba15f39c179a5017571f99d8
Matteo Varnier <matvarnier@gmail.com>
2023-11-28 00:37:09 -0300

    commit

--- Projeto2/Escada_rolante.c
+++ Projeto2/Escada_rolante.c
@@ -4,19 +4,21 @@
 
 // Definição da estrutura Pessoa
 typedef struct Pessoa {
-    int chegada;
-    int sentido;
-    int chegou;
+    int chegada; //tempo de chegada da pessoa
+    int sentido;    //sentido em que ela vai(0 ou 1)
+    int chegou;     //se a pessoa chegou ao outro lado
     struct Pessoa* next;
 } Pessoa;
 
 // Função para criar um novo nó (Pessoa)
 Pessoa* criarPessoa(int chegada, int sentido) {
+    //aloca a memoria
     Pessoa* novaPessoa = (Pessoa*)malloc(sizeof(Pessoa));
     if (novaPessoa == NULL) {
         printf("Erro na alocação de memória.\n");
         exit(EXIT_FAILURE);
     }
+    //inicializa o ponteiro
     novaPessoa->chegada = chegada;
     novaPessoa->sentido = sentido;
     novaPessoa->next = NULL;
@@ -26,8 +28,10 @@ Pessoa* criarPessoa(int chegada, int sentido) {
 // Função para adicionar um nó (Pessoa) no final da lista
 void adicionarAoFimLista(Pessoa** head, Pessoa* novaPessoa) {
     if (*head == NULL) {
+        //lista vazia, vira o topo da lista
         *head = novaPessoa;
     } else {
+        //se a lista nao estiver vazia, vai ate o final e adiciona o novo no
         Pessoa* atual = *head;
         while (atual->next != NULL) {
             atual = atual->next;
@@ -39,6 +43,7 @@ void adicionarAoFimLista(Pessoa** head, Pessoa* novaPessoa) {
 // Função para remover o nó (Pessoa) no início da lista
 void removerDoInicio(Pessoa** head) {
     if (*head != NULL) {
+        //remove o no do topo e libera memoria
         Pessoa* temp = *head;
         *head = (*head)->next;
         free(temp);
@@ -49,12 +54,13 @@ void removerDoInicio(Pessoa** head) {
 void imprimirLista(Pessoa* head) {
     Pessoa* atual = head;
     while (atual != NULL) {
-        printf("Chegada: %d, Sentido: %d -> ", atual->chegada, atual->sentido);
+        //imprimi os dados. Qual o tempo de chegada e o sentido em que ela vai
+        printf("%d %d \n", atual->chegada, atual->sentido);
         atual = atual->next;
     }
-    printf("NULL\n");
+    
 }
-
+//limpar toda a lista
 void limpaLista(Pessoa** head) {
     while (*head != NULL) {
         removerDoInicio(head);
@@ -67,47 +73,52 @@ int main() {
     Pessoa* pessoas = NULL;
 
     srand(time(NULL));
-
+    
     printf("Digite o número de pessoas: ");
-    scanf("%d", &N);
+    scanf("%d", &N);   //le o numero de pessoas que ira na esca rolante
 
-    printf("Digite os tempos de chegada e direções para cada pessoa:\n");
-
+    if (N > 100000) {
+        printf("Numero de pessoas nao cabe na escada rolante.\n");
+        return 1; // Termina o programa com código de erro 1
+    }
+    
+    int ultimo_valor = 0;
+    
+    //loop para gerar pessoas com tempos aleatorios e o sentido variar entre 0 e 1
     for (i = 0; i < N; i++) {
         int chegada, sentido;
-        printf("Tempo de chegada para a pessoa %d: ", i + 1);
-        scanf("%d", &chegada);
+        chegada = ultimo_valor + (rand() % 10) + 1; //tempo aleatorio 
+        ultimo_valor = chegada;
 
-        printf("Sentido para a pessoa %d (0 para esquerda, 1 para direita): ", i + 1);
-        scanf("%d", &sentido);
+        sentido = rand() %2;
 
         Pessoa* novaPessoa = criarPessoa(chegada, sentido);
         adicionarAoFimLista(&pessoas, novaPessoa);
     }
 
     tempofinal = 0;
-    limite = 0;
+    limite = 10;
 
-    Pessoa* atual = pessoas;
-
-    while (atual != NULL) {
+    Pessoa* atual = pessoas;    //inicializa o ponteiro para percorrer a lista
+    //loop para processar as chegadas e sentidos e ver o tempo final
+    while(atual != NULL){
         if (atual->chegou == 0) {
             if (tempofinal == 0) {
-                tempofinal = atual->chegada + 10;
+                tempofinal = atual->chegada+10;
             } else {
                 tempofinal += 10;
             }
             atual->chegou = 1;
         }
-
+        //loop para ver as restricoes do programa
         for (Pessoa* j = atual->next; j != NULL; j = j->next) {
             int diff = j->chegada - atual->chegada;
             if (limite - diff > 0 || tempofinal > j->chegada) {
                 if (atual->sentido == j->sentido && j->chegou == 0) {
-                    if (tempofinal > j->chegada + 10) {
-                        continue;
+                    if (tempofinal > j->chegada+10) {
+                        tempofinal = tempofinal;
                     } else {
-                        tempofinal = j->chegada + 10;
+                        tempofinal = j->chegada +10;
                     }
                     limite = 10;
                     j->chegou = 1;
@@ -122,11 +133,12 @@ int main() {
         atual = atual->next;
     }
 
+    //imprime a lista
     imprimirLista(pessoas);
-    printf("Tempo final: %d\n", tempofinal);
+    printf("Tempo final: %d\n", tempofinal);//retorna o tempo final
 
     // Liberando a memória alocada para as listas
-    limpaLista(&pessoas);
+    limpaLista(&pessoas);//libera memoria
 
     return 0;
 }
---
+++ Projeto2/README.md
@@ -0,0 +1,58 @@
+# Grupo_Aws
+É o grupo Milf Hunters Sistemas Operacionais
+
+# Integrantes
+
+- Matteo Domiciano Varnier
+- Felipe Mazzeo Barbosa
+- Daniel Reis Raske
+
+# Sobre o Projeto
+
+Nesse projeto tivemos o desafio de solucionar um problema de uma escada rolante dupla, aonde somente uma escada rolante sobe e desce.
+
+O codigo pode quantas pessoas estaram na escada rolante. 
+Ao a primeira pessoa entrar, ele ira verificar se a proxima pessoa que esta chegando ira para a mesma posicao ou nao.
+
+Se a pessoa for ao mesmo lado, a escada rolante sobe e com isso so somamos 10 na ultima pessoa da escada
+
+Se a pessoa for para o outro lado, o codigo bloqueia essa pessoa e faz com que a espere que todos cheguem ao outro lado para a pessoa atravessar. 
+
+# Sistema Operacional
+![alt](assets/Sistema.png)
+
+## Compilar
+1. Clone o repositorio com o seguinte comando:
+    https://github.com/Bobertkiller/Grupo_Aws.git
+2. Acesse a pasta onde foi escolhido para dar o clone do git. Use `cd` para acessar a pasta.
+
+    *Exemplo*:
+
+    ![alt](/Projeto2/assets/cd.png)
+
+    ***feito no prompt de comando do linux***
+3. Digite `gcc` para poder compilar o codigo
+
+    *Exemplo*:
+
+    ![alt](/Projeto2/assets/gcc.png)
+
+    ***feito no prompt de comando do linux***
+3. Digite `./nome do arquivo` para poder rodar o codigo
+
+    *Exemplo*:
+
+    ![alt](/Projeto2/assets/barra.png)
+
+    ***feito no prompt de comando do linux***
+# Testes de mesa
+###  Teste de mesa com 5 pessoas
+![alt](/Projeto2/assets/print4.png)
+
+###  Teste de mesa com 10 pessoas 
+![alt](/Projeto2/assets/print1.png)
+
+### Teste de mesa com 100 pessoas
+
+![alt](/Projeto2/assets/print2.png)
+![alt](/Projeto2/assets/print3.png)
